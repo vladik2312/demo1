99249beda3cef30f211e8403de21f5fdd74ba25d
Voland <Tereshenkovladik@gmail.com>
2024-03-31 15:06:23 +0300

    Стабільна версія 3

--- src/main/java/com/example/demo/DeleteService.java
+++ src/main/java/com/example/demo/DeleteService.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
 public class DeleteService {
+    private final MySQLConnection mySQLConnection;
+
+    public DeleteService(MySQLConnection mySQLConnection) {
+        this.mySQLConnection = mySQLConnection;
+    }
 
     public void deleteRecord(String id) {
-        // Логіка видалення запису за ідентифікатором "id"
+        mySQLConnection.deleteRecord(id);
     }
 }
--- src/main/java/com/example/demo/ReadService.java
+++ src/main/java/com/example/demo/ReadService.java
@@ -1,8 +1,14 @@
 package com.example.demo;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService
 public class ReadService {
 
-    public String getRecordById(String id) {
+    @WebMethod
+    public String getRecordById(@WebParam(name = "id") String id, @WebParam(name = "column") String column) {
         // Логіка отримання запису за ідентифікатором "id"
         return "Record with ID: " + id;
     }
